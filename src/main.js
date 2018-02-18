292c2edc6d8ce77a4e5771a4fe19cf1fab538c7e
Carlos Hincapié <carmihinfer@gmail.com>
2018-02-18 15:20:26 -0300

    Fix header navbar and responsive background

--- src/main.js
+++ src/main.js
@@ -1,3 +1,19 @@
 window.$ = window.jQuery = require('jquery');
 import 'bootstrap';
 import './assets/css/main.scss';
+
+$(document).ready(function() {
+	var after = false;
+	var navbar = $('#navbar');
+	$(window).on('scroll', function(event) {
+		event.preventDefault();
+		console.log('scrollY:' + window.scrollY);
+		if (!after && window.scrollY > 12) {
+			after = true;
+			navbar.toggleClass('navbar-after');
+		} else if (after && window.scrollY < 12) {
+			after = false;
+			navbar.toggleClass('navbar-after');
+		}
+	});
+});
